570efb41d0f1909fee24f6b285a37ba89e458e9b
tiffiany <tiffianyaa_yqq@163.com>
2018-11-16 11:23:00 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -29,6 +29,10 @@ $(function(){
             max: 6,
             message: '用户名长度必须在2到6之间'
           },
+          // 配置回调函数的提示信息
+          callback: {
+            message:"用户名不存在"
+          }
         }
       },
       password: {
@@ -40,6 +44,9 @@ $(function(){
             min: 6,
             max: 12,
             message: '密码长度必须在6到12之间'
+          },
+          callback: {
+            message:"密码错误"
           }
         }
       }
@@ -48,42 +55,48 @@ $(function(){
 
   /*
   * 2. 表单校验需要在表单提交时, 进行校验, 需要submit按钮
-  *    可以注册一个表单校验成功事件(success.form.bv), 表单校验成功之后, 默认会提交
+  *    可以注册一个表单校验成功事件(success.form.bv)插件中定义的事件, 表单校验成功之后, 默认会提交
   *    可以在成功事件中, 阻止默认的表单提交, 通过 ajax 提交, 就不会跳转了
   *
   * 思路: 1. 注册表单校验成功事件
   *      2. 在事件中, 阻止默认的表单提交, 通过 ajax 提交即可
   * */
-  // $('#form').on('success.form.bv',function(e){
-  //   //阻止默认的表单提交
-  //   e.preventDefault();
-  //   $.ajax({
-  //     type:'post',
-  //     url:'/employee/employeeLogin',
-  //     data:$('#form').serialize(),
-  //     dataType:'json',
-  //     success:function(info){
-  //       console.log(info);
-  //       if(info.success) {
-  //         location.href = "index.html";
-  //       }
-  //       //用户名不存在的情况
-  //       if(info.error === 1000) {
-  //         // 调用插件提供的方法, 将用户名input状态 更新成校验失败状态
-  //         // updateStatus
-  //         // 参数1: 校验字段  username/password
-  //         // 参数2: 校验状态  NOT_VALIDATED(未校验), VALIDATING(校验中), INVALID(失败) or VALID(成功)
-  //         // 参数3: 校验规则, 用来配置错误时的提示信息
-  //         $("#form").data('bootstrapValidator').updateStatus('username', 'INVALID',"callback");
-  //       }
-  //       //密码错误的情况
-  //       if(info.error === 1001) {
-  //         $("#form").data('bootstrapValidator').updateStatus('password', 'INVALID',"callback");
-  //       }
-  //     }
-  //   });
-  // });
+  $('#form').on('success.form.bv',function(e){
+    //阻止默认的表单提交
+    e.preventDefault();
+    $.ajax({
+      type:'post',
+      url:'/employee/employeeLogin',
+      // 表单序列化,快速收集 设置了name的表单元素值
+      data:$('#form').serialize(),
+      dataType:'json',
+      success:function(info){
+        console.log(info);
+        // 登录成功
+        if(info.success) {
+          location.href = "index.html";
+        }
+        //用户名不存在的情况
+        if(info.error === 1000) {
+          // 调用插件提供的方法, 将用户名input状态 更新成校验失败状态
+          // updateStatus
+          // 参数1: 校验字段  username/password
+          // 参数2: 校验状态  NOT_VALIDATED(未校验), VALIDATING(校验中), INVALID(失败) or VALID(成功)
+          // 参数3: 校验规则, 用来配置错误时的提示信息
+          $("#form").data('bootstrapValidator').updateStatus('username', 'INVALID',"callback");
+        }
+        //密码错误的情况
+        if(info.error === 1001) {
+          $("#form").data('bootstrapValidator').updateStatus('password', 'INVALID',"callback");
+        }
+      }
+    });
+  });
   /*
-  * 3. 重置功能, reset按钮, 本身就可以重置内容, 这边只需要再重置状态即可
+  * 3. 重置功能, reset按钮, ★本身就可以重置内容★, 这边只需要再重置状态即可
   * */
+ $('[type="reset"]').click(function(){
+   // 传 boolean 值, 如果是 true 内容和状态都重置, 不传参, 只重置状态
+  $('#form').data('bootstrapValidator').resetForm();//只重置状态
+ });
 })
