3debe01beef55430f577679a730c94cbdf5ac1b8
tiffiany <tiffianyaa_yqq@163.com>
2018-11-17 21:38:45 +0800

    公共功能退出功能和登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -24,3 +24,40 @@ $( document ).ajaxStop(function() {
     NProgress.done();
   },500);
 });
+
+// jquery入口函数,会等待DOM结构加载完成之后,就执行
+$(function(){
+  // 公共的功能
+  // 功能1:导航点击切换功能
+  $('.lt_aside .category').click(function(){
+    // 让它下一个兄弟元素child切换显示隐藏
+    $(this).next().stop().slideToggle();
+  });
+  // 功能2:左侧菜单列表切换功能
+  $('.lt_main .icon_left').click(function(){
+    $('.lt_aside').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+    $('.lt_main .lt_topbar').toggleClass('hidemenu');
+  });
+  // 功能3: 退出功能
+  $('.lt_main .icon_right').click(function(){
+    //显示模态框
+    $('#myModal').modal('show');
+  });
+
+  // 模态框的按钮点击事件
+  $('#logoutBtn').click(function(){
+    // 发送ajax请求, 让后台销毁当前用户的登录状态
+    $.ajax({
+      type:'get',
+      url:'/employee/employeeLogout',
+      dataType:'json',
+      success:function(info){
+        if(info.success) {
+          location.href='login.html'
+        }
+      }
+    });
+    // 错误方式:location.href='login.html';
+  });
+});
